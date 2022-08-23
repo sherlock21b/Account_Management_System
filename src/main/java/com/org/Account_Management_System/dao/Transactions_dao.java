b7de8772e7b8cdc62090caf52e2c8aa1f1a1cc41
Rititk Banerjee <banerjeeritik@gmail.com>
2022-08-23 21:51:53 +0530

    Account_Transactions

---
+++ src/main/java/com/org/Account_Management_System/dao/Transactions_dao.java
@@ -0,0 +1,24 @@
+package com.org.Account_Management_System;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Transactions_dao {
+	@Autowired
+	Transactions_repo repo;
+
+	public List<Transactions> showTransactions(){
+		return repo.showTransactions();
+	}
+	
+	public List<Transactions> get_statement(LocalDate date1,LocalDate date2,int acc){
+		return repo.get_statement(date1, date2, acc);
+	}
+}
