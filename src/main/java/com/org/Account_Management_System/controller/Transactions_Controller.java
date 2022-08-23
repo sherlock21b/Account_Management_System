c3343f4a9a8dc7878d2b2fa01c6ca0072a74d560
Rititk Banerjee <banerjeeritik@gmail.com>
2022-08-23 21:50:50 +0530

    Account_Transactions

---
+++ src/main/java/com/org/Account_Management_System/controller/Transactions_Controller.java
@@ -0,0 +1,32 @@
+package com.org.Account_Management_System;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Transactions_Controller {
+	@Autowired
+	Transactions_service service;
+	
+	@GetMapping("/latest-transactions")
+	public List<Transactions> showTransactions(){
+		System.out.println("DOneee");
+		return service.showTransactions();
+	}
+	
+	@GetMapping("/get-statement")
+	public List<Transactions> get_statement(@RequestParam("date1") String date1,@RequestParam("date2")String date2,@RequestParam("acc")int acc){
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate d1=LocalDate.parse(date1,formatter);
+		LocalDate d2=LocalDate.parse(date2,formatter);
+		return service.get_statement(d1, d2, acc);
+	}
+
+}
