ebd79a8f5b421c441e6db39b62c780d9a7963e0b
Rititk Banerjee <banerjeeritik@gmail.com>
2022-08-23 21:57:28 +0530

    Account_Transactions

---
+++ src/main/java/com/org/Account_Management_System/repo/Transactions_repo.java
@@ -0,0 +1,18 @@
+package com.org.Account_Management_System;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+public interface Transactions_repo extends JpaRepository<Transactions, Integer>{
+
+	@Query(value="SELECT * FROM account_transaction LIMIT 5",nativeQuery = true)
+	List<Transactions> showTransactions();
+	
+	@Query(value="SELECT * FROM account_transaction b WHERE b.time BETWEEN ? AND ? AND account_number=?",nativeQuery = true)
+	List<Transactions> get_statement(LocalDate date1,LocalDate date2,int acc);
+}
