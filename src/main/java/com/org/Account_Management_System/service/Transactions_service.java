f759bbec30646334e39e9aa8ff91c526fe534aa0
Rititk Banerjee <rb8751@srmist.edu.in>
2022-08-23 22:32:53 +0530

    Account_Transaction_Changes

--- src/main/java/com/org/Account_Management_System/service/Transactions_service.java
+++ src/main/java/com/org/Account_Management_System/service/Transactions_service.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Service;
 public class Transactions_service {
 	@Autowired
 	Transactions_dao dao;
-	public List<Transactions> showTransactions(){
-		return dao.showTransactions();
+	public List<Transactions> showTransactions(int acc){
+		return dao.showTransactions(acc);
 	}
 	public List<Transactions> get_statement(LocalDate date1,LocalDate date2,int acc){
 		return dao.get_statement(date1, date2, acc);
