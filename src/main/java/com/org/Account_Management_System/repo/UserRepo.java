14d999244c5aab5c5678fa8b2572710ba65f9e23
Hardik <ronnieclave@gmail.com>
2022-08-24 14:56:12 +0530

    Login part

--- src/main/java/com/org/Account_Management_System/repo/UserRepo.java
+++ src/main/java/com/org/Account_Management_System/repo/UserRepo.java
@@ -1,11 +1,20 @@
 package com.org.Account_Management_System.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 
 import com.org.Account_Management_System.dto.User;
 
-public interface UserRepo extends JpaRepository<User, Integer>{
+import java.util.List;
 
+public interface UserRepo extends JpaRepository<User, Integer>{
+	@Query( value="select * from ams.user b where b.user_id=?",nativeQuery = true )
+	User findByUserId(int user_id);
+	
+	@Query( value="select b.name from ams.customer b where b.customer_id=?",nativeQuery = true )
+	String getCustomerNameById(int customer_id);
+	
+	@Query( value="select * from ams.bank_account b where b.customer_id=?",nativeQuery = true )
+	List<Object[]> getAccountDetailsById(int customer_id);
 
 }
